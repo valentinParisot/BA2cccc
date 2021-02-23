9754d6fb0731bf431499dd2114869d1464d4e09d
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-02-23 18:19:25 +0100

    j

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -4,31 +4,31 @@ import java.util.List;
 
 public enum Card {
 
-    BLACK(1),
-    VIOLET(2),
-    BLUE(3),
-    GREEN(4),
-    YELLOW(5),
-    ORANGE(6),
-    RED(7),
-    WHITE(8),
+    BLACK(Color.BLACK),
+    VIOLET(Color.VIOLET),
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN),
+    YELLOW(Color.YELLOW),
+    ORANGE(Color.ORANGE),
+    RED(Color.RED),
+    WHITE(Color.WHITE),
     LOCOMOTIVE(null);
 
-    public static final List<Card> ALL = List.of(values());
+    public static final List<Card> ALL =  List.of(values());
     public static final int COUNT = ALL.size();
     public final static List<Card> CARS =  ALL.subList(0,COUNT);
+    private final Color c;
 
+    private Card(Color b) {
+        this.c = b;
+    }
 
 
     public Card of (Color color){
         return Card.valueOf(color.toString());
     }
     public Color color(){
-
-    return
-
-
-
+    return c;
     }
 
 }
