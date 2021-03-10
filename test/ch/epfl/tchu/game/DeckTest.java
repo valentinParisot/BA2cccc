e4ba0b2f3a2636e63fb8b49b0ef8e9d2f2441478
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-03-10 17:48:06 +0100

    check et test

---
+++ test/ch/epfl/tchu/game/DeckTest.java
@@ -0,0 +1,32 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DeckTest {
+
+
+
+public static final Random
+    non_random = new Random() {
+    @Override
+    public int nextInt (int i){
+        return i-1;
+    }
+};
+
+
+
+    @Test
+    void topcardre() {
+        Deck d;
+        d = Deck.of(SortedBag.of(List.of(1,2,3)), non_random );
+        System.out.print(d.topCard());
+
+    }
+}
