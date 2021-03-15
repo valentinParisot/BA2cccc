f1b2280890d95a3205dffde20600a5748ec485e8
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-03-15 13:40:12 +0100

    SPB

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -2,6 +2,7 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class StationPartition implements StationConnectivity{
@@ -24,14 +25,29 @@ public final class StationPartition implements StationConnectivity{
         return false;
     }
 
+
     public final static class Builder{
+        private List<Integer> integersBuilder;
 
         public Builder(int stationCount){
             Preconditions.checkArgument(stationCount>=0);
-
+            integersBuilder = new ArrayList<Integer>(stationCount) {};
+            for(Integer i : integersBuilder){
+                integersBuilder.set(i, i);
+            }
         }
+
         private int representative(int id){
-            return 1;
+            return integersBuilder.get(id);
+        }
+
+        public Builder connect(Station s1, Station s2){
+            integersBuilder.set(s2.id(), representative(s1.id()));
+            return this;
+        }
+
+        public StationPartition build(){
+            return new StationPartition(integersBuilder);
         }
     }
 
