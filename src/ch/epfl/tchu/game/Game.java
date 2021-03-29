0037f9abbc515ca6df816b033a43a543bea85273
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-03-29 17:11:17 +0200

    class Game

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.gui.Info;
 
@@ -7,24 +8,82 @@ import java.util.*;
 
 public final class Game {
 
-    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng){
+    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
 
-        Player player1 = players.get(PlayerId.PLAYER_1);
-        Player player2 = players.get(PlayerId.PLAYER_2);
-        String name1 = playerNames.get(PlayerId.PLAYER_1);
-        String name2 = playerNames.get(PlayerId.PLAYER_2);
-        Info i1 = new Info(name1);
-        Info i2 = new Info(name2);
+        Preconditions.checkArgument((players.size() == 2) && (playerNames.size() == 2));
 
+        GameState myState = GameState.initial(tickets, rng);
+        Info i1 = new Info(playerNames.get(PlayerId.PLAYER_1));
+        Info i2 = new Info(playerNames.get(PlayerId.PLAYER_2));
+
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            entry.getValue().initPlayers(entry.getKey(), playerNames);
+        }
+
+        sendInfo(new Info(playerNames.get(myState.currentPlayerId())).willPlayFirst(), players);
+
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            entry.getValue().setInitialTicketChoice(myState.topTickets(5));
+            myState = myState.withoutTopTickets(5);
+        }
+
+        informState(myState, players);
+
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            myState = myState.withInitiallyChosenTickets(entry.getKey(), entry.getValue().chooseInitialTickets());
+        }
+
+        sendInfo(i1.keptTickets(myState.playerState(PlayerId.PLAYER_1).ticketCount()), players);
+        sendInfo(i2.keptTickets(myState.playerState(PlayerId.PLAYER_2).ticketCount()), players);
+
+        Player current = players.get(myState.currentPlayerId());
+        switch (current.nextTurn()) {
+            case DRAW_TICKETS: // besoin d'appeler chooseTickets de Player autrement?
+                myState = myState.withChosenAdditionalTickets(myState.topTickets(3),
+                        current.chooseTickets(myState.topTickets(3)));
+                break;
+
+            case DRAW_CARDS:
+                int n = 0;
+                do {
+                    int d1 = current.drawSlot();
+                    if (0 <= d1 && d1 <= 4) {
+                        myState = myState.withDrawnFaceUpCard(d1);
+                    } else if (d1 == Constants.DECK_SLOT) {
+                        myState = myState.withBlindlyDrawnCard();
+                    }
+                    n++;
+                } while (n <= 1);
+                break;
+
+            case CLAIM_ROUT:
+                Route route = current.claimedRoute();
+                SortedBag<Card> initialClaimCards = current.initialClaimCards();
+                SortedBag.Builder<Card> additional = new SortedBag.Builder<>();
+                for (int i = 0; i < 3; ++i) {
+                    additional.add(myState.topCard());
+                    myState = myState.withoutTopCard();
+                }
+
+                if (route.level() == Route.Level.UNDERGROUND &&
+                        route.additionalClaimCardsCount(initialClaimCards, additional.build()) < 1 &&
+                ) {
+
+                }
+        }
 
     }
 
-    private void sendInfo(String info, Player p1, Player p2){
-        p1.receiveInfo(info);
-        p2.receiveInfo(info);
+
+    private static void sendInfo(String info, Map<PlayerId, Player> players) {
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            entry.getValue().receiveInfo(info);
+        }
     }
-    private void informState(PublicGameState newState, Player p1,PlayerState ps1, Player p2, PlayerState ps2){
-        p1.updateState(newState, ps1);
-        p2.updateState(newState,ps2);
+
+    private static void informState(GameState newState, Map<PlayerId, Player> players) {
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            entry.getValue().updateState(newState, newState.playerState(entry.getKey()));
+        }
     }
 }
