698abf1a7dd7fc7a651359b453d2b3e571eee1de
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-03-31 13:32:50 +0200

    erreur dans sign check c'est ta classe

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -37,6 +37,7 @@ public final class Game {
         sendInfo(i2.keptTickets(myState.playerState(PlayerId.PLAYER_2).ticketCount()), players);
 
         Player current = players.get(myState.currentPlayerId());
+
         switch (current.nextTurn()) {
             case DRAW_TICKETS: // besoin d'appeler chooseTickets de Player autrement?
                 myState = myState.withChosenAdditionalTickets(myState.topTickets(3),
@@ -60,16 +61,50 @@ public final class Game {
                 Route route = current.claimedRoute();
                 SortedBag<Card> initialClaimCards = current.initialClaimCards();
                 SortedBag.Builder<Card> additional = new SortedBag.Builder<>();
-                for (int i = 0; i < 3; ++i) {
-                    additional.add(myState.topCard());
-                    myState = myState.withoutTopCard();
+
+
+                if (route.level() == Route.Level.UNDERGROUND) {
+                    for (int i = 0; i < 3; ++i) {
+                        additional.add(myState.topCard());
+                        myState = myState.withoutTopCard();
+                    }
+
+                    SortedBag<Card> c = additional.build();
+
+                    // 3 cas quand c'est un tunnel
+                    // a)tu pioches des cartes et tu as des cartes additionelles et tu peux t'emparr de la routw
+                    // b) pareil mais peut pas t'emparer de la route
+                    // c) pareil mais tu as pas de cartes additionelle et ut peuxc t'emprwer de la route
+
+                    if(route.additionalClaimCardsCount(initialClaimCards, additional.build()) >= 1 && !current.chooseAdditionalCards(List.of(c)).isEmpty()){
+
+                        canclaimedroute
+
+                    }
+
+
+                    if(){
+
+                    }
+                    if(route.additionalClaimCardsCount(initialClaimCards, additional.build()) >= 1 && current.chooseAdditionalCards(List.of(c)).isEmpty()){
+
+
+                    }
+
+                    }
+
+                if (route.level() == Route.Level.OVERGROUND){
+
+                    myState = myState.withClaimedRoute(route,initialClaimCards);
                 }
 
-                if (route.level() == Route.Level.UNDERGROUND &&
-                        route.additionalClaimCardsCount(initialClaimCards, additional.build()) < 1 &&
-                ) {
+
+
+                // route tu t'empares
+                //overgroudn acutalise gamestate en disant que il prend la route
 
                 }
+
         }
 
     }
