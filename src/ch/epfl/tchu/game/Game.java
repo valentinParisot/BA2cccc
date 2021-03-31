6745aa466068c8b3c8405e3375ec0cc3225af594
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-03-31 15:22:49 +0200

    erreur dans sign check c'est ta classe

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -36,77 +36,84 @@ public final class Game {
         sendInfo(i1.keptTickets(myState.playerState(PlayerId.PLAYER_1).ticketCount()), players);
         sendInfo(i2.keptTickets(myState.playerState(PlayerId.PLAYER_2).ticketCount()), players);
 
-        Player current = players.get(myState.currentPlayerId());
 
-        switch (current.nextTurn()) {
-            case DRAW_TICKETS: // besoin d'appeler chooseTickets de Player autrement?
-                myState = myState.withChosenAdditionalTickets(myState.topTickets(3),
-                        current.chooseTickets(myState.topTickets(3)));
-                break;
+        do {
+            Player current = players.get(myState.currentPlayerId());
+            switch (current.nextTurn()) {
+                case DRAW_TICKETS: // besoin d'appeler chooseTickets de Player autrement?
+                    myState = myState.withChosenAdditionalTickets(myState.topTickets(3),
+                            current.chooseTickets(myState.topTickets(3)));
+                    break;
 
-            case DRAW_CARDS:
-                int n = 0;
-                do {
-                    int d1 = current.drawSlot();
-                    if (0 <= d1 && d1 <= 4) {
-                        myState = myState.withDrawnFaceUpCard(d1);
-                    } else if (d1 == Constants.DECK_SLOT) {
-                        myState = myState.withBlindlyDrawnCard();
+                case DRAW_CARDS:
+                    int n = 0;
+                    do {
+                        int d1 = current.drawSlot();
+                        if (0 <= d1 && d1 <= 4) {
+                            myState = myState.withDrawnFaceUpCard(d1);
+                        } else if (d1 == Constants.DECK_SLOT) {
+                            myState = myState.withBlindlyDrawnCard();
+                        }
+                        n++;
+                    } while (n <= 1);
+                    break;
+
+                case CLAIM_ROUT:
+                    Route route = current.claimedRoute();
+                    SortedBag<Card> initialClaimCards = current.initialClaimCards();
+                    SortedBag.Builder<Card> additional = new SortedBag.Builder<>();
+
+
+                    if (route.level() == Route.Level.UNDERGROUND) {
+                        for (int i = 0; i < 3; ++i) {
+                            myState = myState.withCardsDeckRecreatedIfNeeded(rng);
+                            additional.add(myState.topCard());
+                            myState = myState.withoutTopCard();
+                        }
+
+                        SortedBag<Card> c = additional.build();
+
+                        int adittionalcardscount = route.additionalClaimCardsCount(initialClaimCards, c);
+                        List<SortedBag<Card>> possibleAdditionalCards = myState.currentPlayerState().possibleAdditionalCards(adittionalcardscount, initialClaimCards, c);
+
+                        //contains ? ou empty
+                        if (route.additionalClaimCardsCount(initialClaimCards, c) >= 1 &&
+                                !possibleAdditionalCards.isEmpty()) {
+
+                            SortedBag<Card> currentAditional = current.chooseAdditionalCards(List.of(c));
+
+                            //pas utile si il choisit rien rien se passe
+                            if (current.chooseAdditionalCards(possibleAdditionalCards).isEmpty()) {
+
+                                myState = myState;
+                            }
+
+                            if (!current.chooseAdditionalCards(possibleAdditionalCards).isEmpty()) {
+
+                                myState = myState.withClaimedRoute(route, current.chooseAdditionalCards(possibleAdditionalCards));
+
+                            }
+
+                        }
+
+                        if (route.additionalClaimCardsCount(initialClaimCards, additional.build()) == 0) {
+
+                            myState = myState.withClaimedRoute(route, initialClaimCards);
+
+                        }
+
                     }
-                    n++;
-                } while (n <= 1);
-                break;
 
-            case CLAIM_ROUT:
-                Route route = current.claimedRoute();
-                SortedBag<Card> initialClaimCards = current.initialClaimCards();
-                SortedBag.Builder<Card> additional = new SortedBag.Builder<>();
+                    if (route.level() == Route.Level.OVERGROUND) {
 
-
-                if (route.level() == Route.Level.UNDERGROUND) {
-                    for (int i = 0; i < 3; ++i) {
-                        additional.add(myState.topCard());
-                        myState = myState.withoutTopCard();
+                        myState = myState.withClaimedRoute(route, initialClaimCards);
                     }
 
-                    SortedBag<Card> c = additional.build();
+            }
 
-                    // 3 cas quand c'est un tunnel
-                    // a)tu pioches des cartes et tu as des cartes additionelles et tu peux t'emparr de la routw
-                    // b) pareil mais peut pas t'emparer de la route
-                    // c) pareil mais tu as pas de cartes additionelle et ut peuxc t'emprwer de la route
+            myState = myState.forNextTurn();
 
-                    if(route.additionalClaimCardsCount(initialClaimCards, additional.build()) >= 1 && !current.chooseAdditionalCards(List.of(c)).isEmpty()){
-
-                       myState = myState.
-
-                    }
-
-
-                    if(){
-
-                    }
-                    if(route.additionalClaimCardsCount(initialClaimCards, additional.build()) >= 1 && current.chooseAdditionalCards(List.of(c)).isEmpty()){
-
-
-                    }
-
-                    }
-
-                if (route.level() == Route.Level.OVERGROUND){
-
-                    myState = myState.withClaimedRoute(route,initialClaimCards);
-                }
-
-
-
-                // route tu t'empares
-                //overgroudn acutalise gamestate en disant que il prend la route
-
-                }
-
-        }
-
+        } while (!myState.currentPlayerId().equals(myState.lastPlayer()));
     }
 
 
