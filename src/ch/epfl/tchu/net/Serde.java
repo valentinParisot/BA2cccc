48aef78e53064117f733f87377709b49f1905890
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-04-12 16:42:54 +0200

    interface Serde

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -1,4 +1,73 @@
 package ch.epfl.tchu.net;
 
-public interface Serde {
+import java.util.List;
+import java.util.function.Function;
+
+public interface Serde<Obj> {
+
+    public abstract String serialize(Obj o);
+
+    public abstract Obj deSerialize(String s);
+
+
+    public static <Obj> Serde of(Function<Obj, String> serialize, Function<String, Obj> deSerialize) {
+        return new Serde<Obj>() {
+            @Override
+            public String serialize(Obj o){
+                return serialize.apply(o);
+            }
+
+            @Override
+            public Obj deSerialize(String s) {
+                return deSerialize.apply(s);
+            }
+        };
+    }
+
+
+    public static <Obj> Serde oneOf(List<Obj> list) {
+        return new Serde<Obj>() {
+            @Override
+            public String serialize(Obj o) {
+                int n = list.indexOf(o);
+                return Integer.toString(n);
+            }
+
+            @Override
+            public Obj deSerialize(String s) { // s sera toujours valide? (il faut un int)
+                int n = Integer.parseInt(s);
+                return list.get(n);
+            }
+        };
+    }
+
+
+    // généricité <List<Obj>> impossible
+    public static <Obj> Serde listOf(Serde<Obj> serde, String separation) {
+        return new Serde<Obj>() {
+            @Override
+            public String serialize(Obj o) {
+                return null;
+            }
+
+            @Override
+            public Obj deSerialize(String s) {
+                return null;
+            }
+        };
+    }
+
+    public static <Obj> Serde bagOf() {
+        return new Serde<Obj>() {
+            @Override
+            public String serialize(Obj o) {
+                return null;
+            }
+
+            @Override
+            public Obj deSerialize(String s) {
+                return null;
+            }
+        };
+    }
 }
