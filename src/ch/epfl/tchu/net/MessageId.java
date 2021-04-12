cbe8da804fb59afa09a75ba8dbffe11680e2bc80
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-04-12 11:48:45 +0200

    z

---
+++ src/ch/epfl/tchu/net/MessageId.java
@@ -0,0 +1,30 @@
+package ch.epfl.tchu.net;
+
+import java.util.List;
+
+/**
+ * MessageId
+ * enum
+ *
+ * @author Valentin Parisot (326658)
+ * @author Hugo Jeannin (329220)
+ */
+
+public enum MessageId {
+
+    INIT_PLAYERS,
+    RECEIVE_INFO,
+    UPDATE_STATE,
+    SET_INITIAL_TICKETS,
+    CHOOSE_INITIAL_TICKETS,
+    NEXT_TURN,
+    CHOOSE_TICKETS,
+    DRAW_SLOT,
+    ROUTE,
+    CARDS,
+    CHOOSE_ADDITIONAL_CARDS;
+
+
+    public static final List<MessageId> ALL = List.of(values());
+    public static final int COUNT = ALL.size();
+}
