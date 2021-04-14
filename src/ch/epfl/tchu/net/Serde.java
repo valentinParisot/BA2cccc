111dfe04ae57a94040cb1d6afece2d9b3aa70cfa
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-04-14 14:36:05 +0200

    interface Serde finie

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -1,7 +1,12 @@
 package ch.epfl.tchu.net;
 
+import ch.epfl.tchu.SortedBag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 public interface Serde<Obj> {
 
@@ -10,7 +15,7 @@ public interface Serde<Obj> {
     public abstract Obj deSerialize(String s);
 
 
-    public static <Obj> Serde of(Function<Obj, String> serialize, Function<String, Obj> deSerialize) {
+    public static <Obj> Serde<Obj> of(Function<Obj, String> serialize, Function<String, Obj> deSerialize) {
         return new Serde<Obj>() {
             @Override
             public String serialize(Obj o){
@@ -25,7 +30,7 @@ public interface Serde<Obj> {
     }
 
 
-    public static <Obj> Serde oneOf(List<Obj> list) {
+    public static <Obj> Serde<Obj> oneOf(List<Obj> list) {
         return new Serde<Obj>() {
             @Override
             public String serialize(Obj o) {
@@ -34,7 +39,7 @@ public interface Serde<Obj> {
             }
 
             @Override
-            public Obj deSerialize(String s) { // s sera toujours valide? (il faut un int)
+            public Obj deSerialize(String s) {
                 int n = Integer.parseInt(s);
                 return list.get(n);
             }
@@ -42,31 +47,44 @@ public interface Serde<Obj> {
     }
 
 
-    // généricité <List<Obj>> impossible
-    public static <Obj> Serde listOf(Serde<Obj> serde, String separation) {
-        return new Serde<Obj>() {
+    public static <Obj> Serde<List<Obj>> listOf(Serde<Obj> serde, String separation) {
+        return new Serde<List<Obj>>() {
             @Override
-            public String serialize(Obj o) {
-                return null;
+            public String serialize(List<Obj> l) {
+                List<String> list = new ArrayList<>();
+                for(Obj o : l){
+                    list.add(serde.serialize(o));
+                }
+                return String.join(separation, list);
             }
 
             @Override
-            public Obj deSerialize(String s) {
+            public List<Obj> deSerialize(String s) {
+                List<String> list = Arrays.asList(s.split(Pattern.quote(separation), -1));
+                List<Obj> objs = new ArrayList<>();
+                for(String string : list){
+                    objs.add(serde.deSerialize(string));
+                }
                 return null;
             }
         };
     }
 
-    public static <Obj> Serde bagOf(Serde<Obj> serde, String separation) {
-        return new Serde<Obj>() {
+    public static <Obj extends Comparable<Obj>> Serde<SortedBag<Obj>> bagOf(Serde<Obj> serde, String separation) {
+        return new Serde<SortedBag<Obj>>() {
+
+            Serde<List<Obj>> listOf = Serde.listOf(serde, separation);
+
             @Override
-            public String serialize(Obj o) {
-                return null;
+            public String serialize(SortedBag<Obj> sb) {
+                List<Obj> list = sb.toList();
+                return listOf.serialize(list);
             }
 
             @Override
-            public Obj deSerialize(String s) {
-                return null;
+            public SortedBag<Obj> deSerialize(String s) {
+                List<Obj> list = listOf.deSerialize(s);
+                return SortedBag.of(list);
             }
         };
     }
