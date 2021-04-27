35eea29674f9aaa43d2e2a3fcb61efac7e4d1d44
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-04-27 09:36:32 +0200

    z

---
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -0,0 +1,112 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.Route;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import javafx.scene.shape.Rectangle;
+
+import java.util.List;
+
+/**
+ * MapViewCreator
+ * class
+ *
+ * @author Valentin Parisot (326658)
+ * @author Hugo Jeannin (329220)
+ */
+
+public abstract class MapViewCreator {
+
+    /**
+     * create the view of the map
+     *
+     * @param observableGameState state of the game observable
+     * @param ObjectProperty      a property containing the action manager to use when the player wants to seize a road
+     * @param cardChooser         card chooser
+     */
+    /**public createMapView(ObservableGameState observableGameState,
+                         ObjectProperty<ClaimRouteHandler> ObjectProperty,
+                         CardChooser cardChooser) {
+
+
+    }**/
+
+    //----------------------------------------------------------------------------------------------------
+
+
+    public static void createMapView() {
+
+        Image image = new Image("map.css");
+        ImageView iv1 = new ImageView(image);
+        final Pane root = new Pane();
+        root.getChildren().setAll(iv1);
+        root.getStylesheets().addAll("map.css", "colors.css");
+
+        Rectangle r1 = new Rectangle();
+        r1.setWidth(36);
+        r1.setHeight(12);
+        r1.getStyleClass().addAll("filled");
+
+        Circle c1 = new Circle();
+        c1.setCenterX(12);
+        c1.setCenterY(6);
+        c1.setRadius(3);
+
+        Circle c2 = new Circle();
+        c2.setCenterX(24);
+        c2.setCenterY(6);
+        c2.setRadius(3);
+
+
+        Rectangle voie = new Rectangle();
+        voie.setWidth(36);
+        voie.setHeight(12);
+        voie.getStyleClass().addAll("track", "filled");
+
+        Group wagon = new Group();
+        wagon.getChildren().addAll(r1, c1, c2);
+        wagon.getStyleClass().addAll("car");
+
+
+        for (Route r : ChMap.routes()) {
+
+            Group routes = new Group();
+            routes.setId(r.id());
+            routes.getStyleClass().addAll("route", "UNDERGROUND", "NEUTRAL");
+
+
+            for (int i = 1; i <= r.length(); i++) {
+                Group box = new Group();
+                box.setId(r.id() + "_" + i);
+                box.getChildren().addAll(voie, wagon);
+                routes.getChildren().add(box);
+            }
+
+            root.getChildren().setAll(routes);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------
+
+
+    @FunctionalInterface
+    interface CardChooser {
+
+        /**
+         * called when the player must choose the cards he wishes to use to seize a road.
+         *
+         * @param options The possibilities open to him
+         *                handler the action handler is intended to be used when he has made his choice
+         */
+        void chooseCards(List<SortedBag<Card>> options/*, ChooseCardsHandler handler */);
+
+    }
+}
+
+
---
+++ test/ch/epfl/tchu/net/Stage9Test.java
@@ -0,0 +1,102 @@
+package ch.epfl.tchu.net;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+import ch.epfl.tchu.gui.MapViewCreator;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public final class Stage9Test extends Application {
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
+        //ObservableGameState gameState = new ObservableGameState(PLAYER_1);
+
+        MapViewCreator.createMapView();
+        final Image image = new Image("map.png");
+        final ImageView imageView = new ImageView(image);
+        final Pane root = new Pane();
+        root.getChildren().setAll(imageView);
+        final Scene scene = new Scene(root, 1000, 1000);
+        primaryStage.setTitle("Test d'affichage d'image");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        /** ObjectProperty<ClaimRouteHandler> claimRoute =
+                new SimpleObjectProperty<>(Stage9Test::claimRoute);
+        ObjectProperty<DrawTicketsHandler> drawTickets =
+                new SimpleObjectProperty<>(Stage9Test::drawTickets);
+        ObjectProperty<DrawCardHandler> drawCard =
+                new SimpleObjectProperty<>(Stage9Test::drawCard);
+
+        Node mapView = MapViewCreator
+                .createMapView(gameState, claimRoute, Stage9Test::chooseCards);
+        Node cardsView = DecksViewCreator
+                .createCardsView(gameState, drawTickets, drawCard);
+        Node handView = DecksViewCreator
+                .createHandView(gameState);
+
+        BorderPane mainPane =
+                new BorderPane(mapView, null, cardsView, handView, null);
+        primaryStage.setScene(new Scene(mainPane));
+        primaryStage.show();
+
+        setState(gameState);**/
+    }
+
+    /** private void setState(ObservableGameState gameState) {
+        PlayerState p1State =
+                new PlayerState(SortedBag.of(ChMap.tickets().subList(0, 3)),
+                        SortedBag.of(1, Card.WHITE, 3, Card.RED),
+                        ChMap.routes().subList(0, 3));
+
+        PublicPlayerState p2State =
+                new PublicPlayerState(0, 0, ChMap.routes().subList(3, 6));
+
+        Map<PlayerId, PublicPlayerState> pubPlayerStates =
+                Map.of(PLAYER_1, p1State, PLAYER_2, p2State);
+        PublicCardState cardState =
+                new PublicCardState(Card.ALL.subList(0, 5), 110 - 2 * 4 - 5, 0);
+        PublicGameState publicGameState =
+                new PublicGameState(36, cardState, PLAYER_1, pubPlayerStates, null);
+        gameState.setState(publicGameState, p1State);
+    }**/
+
+    private static void claimRoute(Route route, SortedBag<Card> cards) {
+        System.out.printf("Prise de possession d'une route : %s - %s %s%n",
+                route.station1(), route.station2(), cards);
+    }
+
+    /**private static void chooseCards(List<SortedBag<Card>> options,
+                                    ChooseCardsHandler chooser) {
+        chooser.onChooseCards(options.get(0));
+    }**/
+
+    private static void drawTickets() {
+        System.out.println("Tirage de billets !");
+    }
+
+    private static void drawCard(int slot) {
+        System.out.printf("Tirage de cartes (emplacement %s)!\n", slot);
+    }
+
+
+    public void start2(Stage primaryStage) {
+
+        final Image image = new Image("map.png");
+        final ImageView imageView = new ImageView(image);
+        final Pane root = new Pane();
+        root.getChildren().setAll(imageView);
+        final Scene scene = new Scene(root, 1000, 1000);
+        primaryStage.setTitle("Test d'affichage d'image");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
