d14b1a65ab6c2167b4f273f40e5c1a9cac6edb3d
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-04-27 10:59:37 +0200

    DecksViewCreator

--- src/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -1,11 +1,31 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.Ticket;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
+
+import java.util.List;
 
 class DecksViewCreator {
 
-    public HBox createHandView(ObservableGameState gameState){
-        return null;
+    public HBox createHandView(ObservableGameState state){
+
+        HBox handView = new HBox();
+        handView.getStylesheets().add("decks.css");
+        handView.getStylesheets().add("colors.css");
+
+        ListView<String> ticketView = new ListView<String>();
+        ticketView.getStyleClass().add("tickets");
+
+        HBox handPane = new HBox();
+        handPane.getStyleClass().add("hand-pane");
+
+        handView.getChildren().add(ticketView);
+        handView.getChildren().add(handPane);
+
+        return handView;
     }
     //public createCardsView()
 
--- src/ch/epfl/tchu/gui/ObservableGameState.java
+++ src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -1,5 +1,18 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.game.PublicGameState;
+import javafx.collections.ObservableList;
+
 public class ObservableGameState {
 
+    private final PlayerId playerId;
+
+    public ObservableGameState(PlayerId playerId){
+        this.playerId = playerId;
+    }
+
+    public ObservableGameState setState(PublicGameState gameState){
+        return null;
+    }
 }
