115c0dc7ed2d35aa9434c6292066b1e1be752983
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-05-11 12:30:32 +0200

    finito

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.net.RemotePlayerClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,12 +9,16 @@ public class ClientMain extends Application {
 
     public static void main(String[] args) {
         launch();
+        // args optionnels?
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        getParameters().getRaw().get(0);
+        RemotePlayerClient client = new RemotePlayerClient(new GraphicalPlayerAdapter(),
+                "localhost",//getParameters().getRaw().get(0),
+                5108);//Integer.parseInt(getParameters().getRaw().get(1)));
 
+        new Thread(() -> client.run()); // juste run()?
     }
 }
