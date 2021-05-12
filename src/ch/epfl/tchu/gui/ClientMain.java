4651436c552b87ea19872fa72c04a6e79fb3a02a
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-05-12 12:05:45 +0200

    finito

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -1,24 +1,45 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.Player;
 import ch.epfl.tchu.net.RemotePlayerClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import static javafx.application.Platform.runLater;
+
 public class ClientMain extends Application {
 
+    private final Player player = new GraphicalPlayerAdapter();
 
     public static void main(String[] args) {
         launch();
-        // args optionnels?
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        RemotePlayerClient client = new RemotePlayerClient(new GraphicalPlayerAdapter(),
-                "localhost",//getParameters().getRaw().get(0),
-                5108);//Integer.parseInt(getParameters().getRaw().get(1)));
+        if (getParameters().getRaw().size() == 0) {
+            RemotePlayerClient client = new RemotePlayerClient(player,
+                    "localhost",
+                    5108);
 
-        new Thread(() -> client.run()); // juste run()?
+            new Thread(() -> client.run()).start();
+
+        } else if (getParameters().getRaw().size() == 1) {
+            RemotePlayerClient client = new RemotePlayerClient(player,
+                    getParameters().getRaw().get(0),
+                    5108);
+
+            new Thread(() -> client.run()).start();
+
+        } else {
+            RemotePlayerClient client = new RemotePlayerClient(player,
+                    getParameters().getRaw().get(0),
+                    Integer.parseInt(getParameters().getRaw().get(1)));
+
+            new Thread(() -> client.run()).start();
+        }
+        // juste run()?
     }
+
 }
