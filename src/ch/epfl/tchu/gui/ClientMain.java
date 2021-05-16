b261f8c7c5107ba93a6ce533e31e9ec15a4ca281
valentinParisot <75210930+valentinParisot@users.noreply.github.com>
2021-05-16 22:25:01 +0200

    graphuplayeradapter quasi finit

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -13,29 +13,10 @@ public class ClientMain extends Application {
         launch(args);
     }
 
-    /**
-    @Override
-    public void start(Stage stage) {
 
-        GraphicalPlayerAdapter player = new GraphicalPlayerAdapter();
 
-        String name = "localhost";
-        int port = 5108;
 
-        if (getParameters().getRaw().size() == 1) {
 
-            name = getParameters().getRaw().get(0);
-
-        } else if (getParameters().getRaw().size() > 1) {
-            name = getParameters().getRaw().get(0);
-            port = Integer.parseInt(getParameters().getRaw().get(0));
-        }
-
-        RemotePlayerClient client = new RemotePlayerClient(player, name, port);
-        new Thread(client::run).start();
-
-    }
-     **/
     @Override
     public void start(Stage primarystage) {
 
