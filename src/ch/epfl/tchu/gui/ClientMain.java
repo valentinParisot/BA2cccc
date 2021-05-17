b7029094fb3f936b398b9c7905868f68f4cb3bf3
JeuneCadreDynamique <hugo.jeannin@epfl.ch>
2021-05-17 15:32:18 +0200

    client juste

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -4,9 +4,8 @@ import ch.epfl.tchu.net.RemotePlayerClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.util.List;
-
 public class ClientMain extends Application {
+
 
 
     public static void main(String[] args) {
@@ -14,26 +13,24 @@ public class ClientMain extends Application {
     }
 
     @Override
-    public void start(Stage primarystage) {
-
-        List<String> arguments = getParameters().getRaw();
-        String name;
-        int port;
-        if(arguments.isEmpty() || arguments.get(0).isEmpty())
-            name = "localhost";
-        else
-            name = arguments.get(0);
-
-        if(arguments.isEmpty() || arguments.get(1).isEmpty())
-            port = 5108;
-        else
-
-            port = Integer.parseInt(arguments.get(1));
+    public void start(Stage stage) {
 
         GraphicalPlayerAdapter player = new GraphicalPlayerAdapter();
+
+        String name = "localhost";
+        int port = 5108;
+
+        if (getParameters().getRaw().size() == 1) {
+
+            name = getParameters().getRaw().get(0);
+
+        } else if (getParameters().getRaw().size() > 1) {
+            name = getParameters().getRaw().get(0);
+            port = Integer.parseInt(getParameters().getRaw().get(0));
+        }
+
         RemotePlayerClient client = new RemotePlayerClient(player, name, port);
-        new Thread(client::run).start();
+        new Thread(() -> client.run()).start();
 
     }
-
 }
